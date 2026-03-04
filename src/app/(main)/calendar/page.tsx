5eb6865a23832e1d59ec05e919e4fe8dae41d917
laoongdao-dev <bimtalao2548@gmail.com>
2026-03-04 12:39:05 +0700

    update

--- src/app/(main)/calendar/page.tsx
+++ src/app/(main)/calendar/page.tsx
@@ -3,6 +3,8 @@
 import { Suspense } from "react"
 import CalendarContent from "./calendar-content"
 
+export const dynamic = "force-dynamic"
+
 export default function CalendarPage() {
   return (
     <Suspense fallback={<div className="p-6">Loading...</div>}>
